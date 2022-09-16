a221ce56d76be15ffcaff4a3b145433265a88f73
David Sanchez <fernandoge210895@gmail.com>
2022-09-15 23:33:00 -0500

    inicializando proyecto

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,12 @@
+import './home.scss';
+import { Navbar } from '../../components/navbar';
+
+const Home = () => {
+    return (
+        <>
+            <Navbar />
+        </>
+    );
+};
+
+export default Home;
